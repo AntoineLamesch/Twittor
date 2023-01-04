be72c0ca5dc9027a755dd6f05c7aaf71c4dce2a2
AntoineLamesch <antoine.lamesch@live.fr>
2023-01-04 15:10:09 +0100

    intro

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -9,7 +9,6 @@ class LuckyController
     public function number(): Response
     {
         $number = random_int(0, 100);
-
         return new Response(
             '<html><body>Lucky number: '.$number.'</body></html>'
         );
